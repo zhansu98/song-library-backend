43684e0652e0d6c80e5106135ec4dfd87a6200cc
zhansu98 <zhansu6598@gmail.com>
2025-03-11 15:30:48 -0600

    Initializing

---
+++ src/test/java/com/songlibrary/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.songlibrary.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
